866d6f579f5aafe7f7619ca002a96ca59c79d534
Kavish Rizvi <kavishrizvi@Kavishs-Mac-mini.local>
2024-05-21 15:59:35 +0530

    Added Kafka Producer

--- src/main/java/org/kavish/training/controller/KafkaController.java
+++ src/main/java/org/kavish/training/controller/KafkaController.java
@@ -1,5 +1,9 @@
 package org.kavish.training.controller;
 
+import org.kavish.training.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/message")
 public class KafkaController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(KafkaController.class);
+    @Autowired
+    private KafkaProducer kafkaProducer;
     @PostMapping
-    public void sendMessage(@RequestParam String message) {
-
+    public String sendMessage(@RequestParam String message) {
+        LOGGER.info("Sending message: {}", message);
+        return kafkaProducer.sendMessage(message);
     }
 }
--- src/main/java/org/kavish/training/producer/KafkaProducer.java
+++ src/main/java/org/kavish/training/producer/KafkaProducer.java
@@ -1,13 +1,26 @@
 package org.kavish.training.producer;
 
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
-import java.util.Properties;
-@Configuration
+@Component
 public class KafkaProducer {
-        //Create Kafka Producer
+    private final static Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+    @Value("${topic.name}")
+    private String topicName;
 
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
+    public String sendMessage(String message) {
+        kafkaTemplate.send(topicName, message);
+        LOGGER.info("Message {} sent to topic {}", message, topicName);
+        return "Message Sent";
+    }
 }
